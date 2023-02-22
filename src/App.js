ca0cbd5212d07f1a5b6a14abc96b18950c53a2ce
riadhramdhani <riadh.etoo@gmail.com>
2023-02-22 11:59:43 +0100

    ka

--- src/App.js
+++ src/App.js
@@ -1,59 +1,47 @@
-import React,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './navbar';
 import Card from './card';
-import  Modal from'./Modal';
-import  Data from'./data';
-import Login from './Login';
+import Data from './data';
+import Home from './Home';
 
-
+import { Route, Routes } from "react-router-dom"
 
 function App() {
 
 
-  
-  var product=JSON.parse(localStorage.getItem("poste"))
-      Data.push(product)
-      var producteupdate=JSON.parse(localStorage.getItem("productes"))
-      if(producteupdate.id===1){
-        Data[0]=producteupdate
 
-      }else if(producteupdate.id===2){
-        Data[1]=producteupdate
+  var product = JSON.parse(localStorage.getItem("poste"))
+  Data.push(product)
+  var producteupdate = JSON.parse(localStorage.getItem("productes"))
+  if (producteupdate.id === 1) {
+    Data[0] = producteupdate
 
-      }else if(producteupdate.id===3){
-        Data[2]=producteupdate
+  } else if (producteupdate.id === 2) {
+    Data[1] = producteupdate
 
-      }else if(producteupdate.id===4){
-        Data[3]=producteupdate
+  } else if (producteupdate.id === 3) {
+    Data[2] = producteupdate
 
-      }
-      else if(producteupdate.id===5){
-        Data[4]=producteupdate
+  } else if (producteupdate.id === 4) {
+    Data[3] = producteupdate
 
-      }
-      
-      const [click,setClick]=useState(false)
+  }
+  else if (producteupdate.id === 5) {
+    Data[4] = producteupdate
+
+  }
+
+  const [click, setClick] = useState(false)
   return (
     <>
-    <div className="App">
-      
-    {click==true? <button onClick={()=>setClick(false)}>logout</button>:null}
-       {click==false ?<Login click={click} setClick={setClick}/>:null}
-    {click==true ? 
-    Data.map((el)=>
-    <div key={el.id}>
-      <Card product={el} />
-     </div>
-    ) :null}
-        
-       
-        
-        
-        <Modal/>
-    </div>
-    
-
+      <Navbar />
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/product' element={Data.map((el) => <Card product={el} />)} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/navbar.js
+++ src/navbar.js
@@ -2,6 +2,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+import  Modal from'./Modal';
 
 function BasicExample() {
   return (
@@ -11,19 +13,11 @@ function BasicExample() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/product">product</Nav.Link>
+
+            <Nav.Link href="#link"><Modal/></Nav.Link>
+          
           </Nav>
         </Navbar.Collapse>
       </Container>
